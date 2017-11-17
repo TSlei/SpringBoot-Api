2d8209ca0dbd77f5fdc1104ae02cf3161c4ed3a5
zhanglei <zhanglei@mmtrix.com>
2017-11-17 18:42:27 +0800

    delete files

--- springboot-application/src/main/java/com/springboot/dao/annotation/UserDao.java
+++ springboot-application/src/main/java/com/springboot/dao/annotation/UserDao.java
@@ -1,4 +1,4 @@
-package com.springboot.dao;
+package com.springboot.dao.annotation;
 
 import java.util.Collection;
 import java.util.List;
--- springboot-application/src/main/java/com/springboot/service/UserService.java
+++ springboot-application/src/main/java/com/springboot/service/UserService.java
@@ -3,7 +3,7 @@ package com.springboot.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.springboot.dao.UserDao;
+import com.springboot.dao.annotation.UserDao;
 import com.springboot.model.UserModel;
 
 @Component
